221310a26527f08296440befba19489d54785d3f
Vitaly Drobyshev <vitaliy.drobyshev@gmail.com>
2019-08-29 16:45:36 -0700

    Adds a counter of whales

--- js/confirm.js
+++
@@ -1,9 +0,0 @@
-
-  if (confirm("You agree to be a part of the solution")) {
-    
-    alert("Excellent. Please proceed");
-  } else {
-    
-    alert("Too bad. Maybe you'll change your mind later");
-  }
-         
--- js/prompt.js
+++
@@ -1,5 +0,0 @@
-var colors = prompt("Please enter a color. ");
-
-var x = document.getElementById("demo");
-
-x.style.color = colors;           
